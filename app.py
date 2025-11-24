068760207a4f3c63058418b4e116fb56fc6073e4
hisbu <43975159+hisbu@users.noreply.github.com>
2025-11-24 08:25:11 +0700

    update

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import requests
 
 app = Flask(__name__)
 title_name="It'ts Movie"
+pages_button = 0
+pages = 1 + pages_button
+API_KEY = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDE5YjliY2E4YjlhY2NjZmY5ODU2NmEzZjc2MDFjZiIsIm5iZiI6MTU2MjUxMjI4Mi44MzM5OTk5LCJzdWIiOiI1ZDIyMGI5YWY0OTVlZTFkOGUzNmI1ZmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VtGJ0l0NGePgt9s-RBM6c5Ue02NQCatQP5Lh79thDUc"
 
-def get_movie_list():
-    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+def get_movie_list(page=1, sort="popularity", order="desc"):
+    urut = f"{sort}.{order}"
+    print(urut)
+    # url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc"
+    url = "https://api.themoviedb.org/3/discover/movie"
+
+    params = {
+        "include_adult": "false",
+        "include_video": "false",
+        "language": "en-US",
+        "sort_by": f"{sort}.{order}",
+        "page": page
+    }
 
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZDE5YjliY2E4YjlhY2NjZmY5ODU2NmEzZjc2MDFjZiIsIm5iZiI6MTU2MjUxMjI4Mi44MzM5OTk5LCJzdWIiOiI1ZDIyMGI5YWY0OTVlZTFkOGUzNmI1ZmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.VtGJ0l0NGePgt9s-RBM6c5Ue02NQCatQP5Lh79thDUc"
+        "Authorization": API_KEY
     }
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, params=params)
 
     return response.json() if response.status_code == 200 else {"results": []}
 
@@ -59,7 +73,30 @@ def get_genre(id):
     
 @app.route("/")
 def home():
-    return render_template('home.html', title=title_name, data=get_movie_list())
+    page = int(request.args.get("page", 1))
+    sort = request.args.get("sort", "popularity")
+    order = request.args.get("order", "desc")
+    data = get_movie_list(page)
+
+    return render_template('home.html', 
+                           title=title_name, 
+                           data=get_movie_list(page, sort, order), 
+                           page=data["page"],
+                           total_pages=data["total_pages"],
+                           sort=sort,
+                           order=order)
+
+    # page = int(request.args.get("page", 1))   # <-- ini yang benar
+
+    # data = get_movie_list(page)
+
+    # return render_template(
+    #     'home.html',
+    #     title=title_name,
+    #     data=data["results"],   # jangan panggil get_movie_list() lagi
+    #     page=data["page"],
+    #     total_pages=data["total_pages"]
+    # )
 
 @app.route("/now")
 def now():
